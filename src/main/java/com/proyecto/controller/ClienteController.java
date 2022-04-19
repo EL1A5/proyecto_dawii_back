97b68baa7cd45e5c1361a2adfdc0d35e63153e6a
AlexanderBern001 <bryan9112@hotmail.com>
2022-04-19 15:57:41 -0500

    crud registar cliente

--- src/main/java/com/proyecto/controller/ClienteController.java
+++ src/main/java/com/proyecto/controller/ClienteController.java
@@ -1,16 +1,54 @@
 package com.proyecto.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto.entidad.Cliente;
+import com.proyecto.service.ClienteService;
 import com.proyecto.util.AppSettings;
 
 @RestController
 @RequestMapping("/url/cliente")
 @CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
 public class ClienteController {
-	
-	//Autor Bryan Bernuy
+	@Autowired
+	private ClienteService cliente;
+
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Cliente>> listaAlumno() {
+		List<Cliente> lista = cliente.listaCliente();
+		return ResponseEntity.ok(lista);
+	}
+
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> insertaAlumno(@RequestBody Cliente obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Cliente objSalida = cliente.insertaActualizaResidente(obj);
+			if (objSalida == null) {
+				salida.put("mensaje","Error al registrar");
+			} else {
+				salida.put("mensaje","exito al registrar");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "Error en el registro");
+		}
+		return ResponseEntity.ok(salida);
+	}
 
 }
+
