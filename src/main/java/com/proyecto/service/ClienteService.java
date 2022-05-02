77fbe44a9ce7c60c30605ca7ce183f1740ee9668
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-05-02 18:47:14 -0500

    Ajustes PC01

--- src/main/java/com/proyecto/service/ClienteService.java
+++ src/main/java/com/proyecto/service/ClienteService.java
@@ -3,7 +3,6 @@ package com.proyecto.service;
 import java.util.List;
 
 import com.proyecto.entidad.Cliente;
-import com.proyecto.entidad.Producto;
 
 public interface ClienteService{
 
--- src/main/java/com/proyecto/service/ClienteServiceImpl.java
+++ src/main/java/com/proyecto/service/ClienteServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.proyecto.entidad.Cliente;
-import com.proyecto.entidad.Producto;
 import com.proyecto.repository.ClienteRepository;
 
 @Service
