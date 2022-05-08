ac51b92534ac60a9ab1d60a3ae285e277f98ab25
AlexanderBern001 <bryan9112@hotmail.com>
2022-05-08 17:51:42 -0500

    crud consultar cliente

--- src/main/java/com/proyecto/controller/ClienteController.java
+++ src/main/java/com/proyecto/controller/ClienteController.java
@@ -3,17 +3,17 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.proyecto.entidad.Cliente;
 import com.proyecto.service.ClienteService;
 import com.proyecto.util.AppSettings;
@@ -54,6 +54,30 @@ public class ClienteController {
 		}
 		return ResponseEntity.ok(salida);
 	}
+	@GetMapping("/listaClienteConParametros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaDocenteNombreDniUbigeo(
+			@RequestParam(name = "nombres", required = false, defaultValue = "") String nombres,
+			@RequestParam(name = "dni", required = false, defaultValue = "") String dni,
+			@RequestParam(name = "idUbigeo", required = false, defaultValue = "-1") int idUbigeo,
+			@RequestParam(name = "estado", required = true, defaultValue = "1") int estado) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			List<Cliente> lista = cliente.listaClientePorNombreDniUbigeo("%"+nombres+"%", dni, idUbigeo, estado);
+			if (CollectionUtils.isEmpty(lista)) {
+				salida.put("mensaje", "No existen registros para mostrar");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Existen " + lista.size() + " registros para mostrar");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje","No se registró, consulte con el administrador");
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+
 }
 
 	
