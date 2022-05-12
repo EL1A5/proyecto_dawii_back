fd2906fd36269523221b33025379e6f3b0a5d7a0
EL1A5 <provinciaelias@gmail.com>
2022-05-11 19:58:00 -0500

    Producto con filtros COMPLETO

--- src/main/java/com/proyecto/controller/ProductoController.java
+++ src/main/java/com/proyecto/controller/ProductoController.java
@@ -1,18 +1,20 @@
 package com.proyecto.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,7 +50,10 @@ public class ProductoController {
 			
 			if (CollectionUtils.isEmpty(listproduct)) {
 				
+				
 				obj.setIdProducto(0);
+				obj.setEstado(1);
+				obj.setFechaRegistro(new Date());
 				Producto objsalida=service.RegistrarProducto(obj);
 				
 				if (objsalida==null) {
@@ -71,5 +76,42 @@ public class ProductoController {
 		return ResponseEntity.ok(salida);
 	}
 	
+	
+	
+	@GetMapping("/porSerie/{numSerie}")
+	@ResponseBody
+	public ResponseEntity<List<Producto>> listaProductoPorNumSerie(@PathVariable("numSerie")String serie) {
+		List<Producto> lista = service.listadoporserie(serie);
+		return ResponseEntity.ok(lista);
+	}
+	
+	@GetMapping("/listaProductosPorFiltros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaDocenteNombreDniUbigeo(
+			@RequestParam(name = "nombre", required = false, defaultValue = "") String nombre,
+			@RequestParam(name = "serie", required = false, defaultValue = "") String serie,
+			@RequestParam(name = "idPais", required = false, defaultValue = "-1") int idPais,
+			@RequestParam(name = "estado", required = true, defaultValue = "1") int estado) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			List<Producto> lista = service.listaProductoPorFiltros("%"+nombre+"%", serie, idPais, estado);
+			if (CollectionUtils.isEmpty(lista)) {
+				salida.put("mensaje", "No existen datos para mostrar");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Existen " + lista.size() + " elementos para mostrar");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "ERROR AL LISTAR");
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+
+	
+	
+	
+
 	
 }
