3af96e6366f5fc81cef3bbaaab822e8245a73835
sebas0104 <sebastian.am345@gmail.com>
2022-05-24 23:35:21 -0500

    Merge branch 'master' of https://github.com/jorgejacinto9701/dawii_2022_1_grupo_04_domingo_backend.git

--- src/main/java/com/proyecto/controller/ReclamoController.java
+++ src/main/java/com/proyecto/controller/ReclamoController.java
@@ -7,11 +7,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,17 +46,54 @@ public class ReclamoController {
 
 			obj.setFechaRegistro(new Date());
 			obj.setEstado(1);
-
 			Reclamo objSalida = reclamoService.insertaActualizaReclamo(obj);
 			if (objSalida == null) {
 				salida.put("mensaje", "No se registró el reclamo");
 			} else {
 				salida.put("mensaje", "Se registró correctamente.");
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			salida.put("mensaje", "No se registró");
 		}
 		return ResponseEntity.ok(salida);
+	
 	}
+
+	
+	
+	@GetMapping("/listaReclamoConParametros")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaReclamoDescricionClienteTiporeclamoFecha(
+			@RequestParam(name = "descripcion",required = false , defaultValue = " ")String descripcion,
+			@RequestParam(name = "idCliente",required = false , defaultValue = "-1")int idCliente,
+			@RequestParam(name = "idTipoReclamo",required = false , defaultValue = "-1")int idTipoReclamo,
+			@RequestParam(name = "fechaCompra",required = false)Date fechaCompra,
+	        @RequestParam(name = "estado", required = true, defaultValue = "1") int estado){
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			if(fechaCompra.getYear()==0) {
+				fechaCompra = null;
+			}
+			List<Reclamo> lista =reclamoService.ListaReclamoDescricionClienteTiporeclamo("%"+ descripcion +"%", idCliente, idTipoReclamo,fechaCompra, estado);
+			
+			if(CollectionUtils.isEmpty(lista)){
+				
+				salida.put("mensaje", "No existen datos para mostrar ");
+			}else {
+				salida.put("lista", lista);	
+				salida.put("mensaje", " Existen " + lista.size() + " para mostrar ");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "No se registró, consulte con el administrador ");
+		}
+		
+		return ResponseEntity.ok(salida);
+
+	
+	}
+	
+	
 }
