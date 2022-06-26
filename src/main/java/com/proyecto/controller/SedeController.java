710fd78e1d8c4323325318c849f52ef5aed8a4f9
HernanCB <hernancardenascb@gmail.com>
2022-06-26 15:45:20 -0500

    commit PC3

--- src/main/java/com/proyecto/controller/SedeController.java
+++ src/main/java/com/proyecto/controller/SedeController.java
@@ -4,13 +4,17 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -82,5 +86,88 @@ public class SedeController {
 		return ResponseEntity.ok(salida);
 
 	}
+	
+	
+	/*===========================CRUD SEDE===================================*/
+	
+	@GetMapping("/listaSedePorNombreLike/{nom}")
+	@ResponseBody
+	public ResponseEntity<List<Sede>> listaDocentePorNombreLike(@PathVariable("nom") String nom) {
+		List<Sede> lista  = null;
+		try {
+			if (nom.equals("todos")) {
+				lista = sedeService.listaSedePorNombreLike("%");
+			}else {
+				lista = sedeService.listaSedePorNombreLike("%" + nom + "%");	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok(lista);
+	}
+	
+	@PostMapping("/registraSede")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> insertaDocente(@RequestBody Sede obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			obj.setIdSede(0);
+			obj.setFechaRegistro(new Date());
+			obj.setEstado(1);
+			Sede objSalida =  sedeService.insertaActualizaSede(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
+			} else {
+				salida.put("mensaje", Constantes.MENSAJE_REG_EXITOSO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+
+	@PutMapping("/actualizaSede")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> actualizaDocente(@RequestBody Sede obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Sede objSalida =  sedeService.insertaActualizaSede(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", Constantes.MENSAJE_ACT_ERROR);
+			} else {
+				salida.put("mensaje", Constantes.MENSAJE_ACT_EXITOSO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", Constantes.MENSAJE_ACT_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	@DeleteMapping("/eliminaSede/{id}")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> eliminaSede(@PathVariable("id")int id) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Optional<Sede> opt = sedeService.buscaSede(id);
+			if (opt.isPresent()) {
+				sedeService.eliminaSede(id);
+				Optional<Sede> optSede = sedeService.buscaSede(id);
+				if (optSede.isEmpty()) {
+					salida.put("mensaje", Constantes.MENSAJE_ELI_EXITOSO);
+				} else {
+					salida.put("mensaje", Constantes.MENSAJE_ELI_ERROR);
+				}
+			}else {
+				salida.put("mensaje", Constantes.MENSAJE_ELI_NO_EXISTE_ID);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", Constantes.MENSAJE_ELI_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
 
 }
